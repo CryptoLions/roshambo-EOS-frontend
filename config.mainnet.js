439951aaae48c5abfef6d398dce31341beb750e6
root <root@dev.cryptolions.io>
2018-09-20 20:39:35 +0300

    conf upd

--- config.mainnet.js
+++ config.mainnet.js
@@ -1,20 +1,20 @@
-var PRODUCTION = false;
+var PRODUCTION = true;
 //var chain = '038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca'; 
 var chain =  'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
 
 var network = {
     blockchain: 'eos',
-    host: 'junglehistory.cryptolions.io',
-    port: 18888,
-    protocol: 'http',
+    host: 'eos.greymass.com',
+    port: 443,
+    protocol: 'https',
     expireInSeconds: 120,
     chainId: chain
 };
 
-var gcontract = "rpstester123";
+var gcontract = "roshambogame";
 
 eos = Eos({
-	httpEndpoint: 'http://junglehistory.cryptolions.io:18888',
+	httpEndpoint: 'https://eos.greymass.com:443',
 	chainId: chain,
 	verbose: false
 });
