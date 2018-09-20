c387c16532133c2cb49daed9346b61c2bba78036
orange1337 <rosman12@gmail.com>
2018-09-20 20:11:00 +0300

    Config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+var PRODUCTION = true;
+var chain = '038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca'; // mainnet 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
+
+var network = {
+    blockchain: 'eos',
+    host: 'junglehistory.cryptolions.io',
+    port: 18888,
+    protocol: 'http',
+    expireInSeconds: 120,
+    chainId: chain
+};
+
+eos = Eos({
+	httpEndpoint: 'http://junglehistory.cryptolions.io:18888',
+	chainId: chain,
+	verbose: false
+});
